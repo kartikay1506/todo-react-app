c04505d43a5ff9f164afbfeefeffae1712f494a3
kartikay1506 <kartikaysawhney150699@gmail.com>
2020-10-01 14:46:58 +0530

    First commit

---
+++ build/precache-manifest.f9b19802315d0f9b35b170ff3aadbe69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd1a565037a3f22c3b97b8c1a836825a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0525161122284420222",
+    "url": "/static/css/main.69cbfd1d.chunk.css"
+  },
+  {
+    "revision": "28b84fa560d10b49858a",
+    "url": "/static/js/2.a508926b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a508926b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0525161122284420222",
+    "url": "/static/js/main.13fef6d8.chunk.js"
+  },
+  {
+    "revision": "e09b3a133424f3eaaad2",
+    "url": "/static/js/runtime-main.d7a593f2.js"
+  }
+]);
